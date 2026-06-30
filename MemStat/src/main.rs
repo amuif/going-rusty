60bc398e29c3c4b6795f7e27e888185348e30863
amuif <amudihayat@gmail.com>
2026-06-30 20:49:37 +0300

    Add incremental build artifacts for MemStat project
    - Created new work-products.bin file to store incremental build data.
    - Added lock file for incremental build management.

--- MemStat/src/main.rs
+++ MemStat/src/main.rs
@@ -1,12 +1,41 @@
-use sysinfo::System;
 use num_format::{Locale, ToFormattedString};
+use sysinfo::System;
 
 fn main() {
     let mut sys = System::new_all();
+    get_ram_size(&sys);
+    get_running_apps(&mut sys);
+}
+
+fn get_ram_size(sys: &System) {
     let availible_ram: u64 = sys.total_memory() / 1024 / 1024;
     let used_ram: u64 = sys.used_memory() / 1024 / 1024;
 
-    sys.refresh_all();
-    println!("Total RAM: {} MB", availible_ram.to_formatted_string(&Locale::en));
+    println!(
+        "Total RAM: {} MB",
+        availible_ram.to_formatted_string(&Locale::en)
+    );
     println!("Used RAM: {} MB", used_ram.to_formatted_string(&Locale::en));
+}
+
+fn get_running_apps(sys: &mut System) {
+    // time to calculate
+    std::thread::sleep(std::time::Duration::from_millis(500));
+    sys.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
+    let mut processes: Vec<_> = sys.processes().values().collect();
+
+    // Sort the processes by memory usage in descending order
+    processes.sort_by(|a, b| b.memory().cmp(&a.memory()));
+
+    println!("{:<10} {:<30} {:<10}", "PID", "Name", "Memory (MB)");
+    println!("{:-<55}", "-");
+    for process in processes.iter().take(15) {
+        let mem_mb: u64 = process.memory() / 1024 / 1024;
+        println!(
+            "{:<10} {:<30} {:<10}",
+            process.pid(),
+            process.name().to_string_lossy(),
+            mem_mb
+        );
+    }
 }
